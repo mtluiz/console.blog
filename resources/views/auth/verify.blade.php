d4b8426a32a57738fedda8662cf1af956c910ad4
Matheus Madureira <matheus.madureira@g.globo>
2022-02-28 01:01:27 -0300

    feat: removing bootstrap from verify email view

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,11 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+
 
                 <div class="card-body">
                     @if (session('resent'))
@@ -20,9 +16,5 @@
                         @csrf
                         <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
                     </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+                
 @endsection
